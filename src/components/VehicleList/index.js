3319ce0d6b2bfac29158ce2e394104d118b6f8a0
Luca Arienti <lucarienti@gmail.com>
2022-07-11 11:40:37 +0200

    feat: added on mount fade staggered animation

--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { motion } from 'framer-motion/dist/framer-motion';
 import useData from './useData';
 import './style.scss';
 import VehicleCard from '../VehicleCard';
+
+const STAGGER = 0.5;
+const duration = 0.5;
 
 export default function VehicleList() {
   const [loading, error, vehicles] = useData();
@@ -15,10 +19,20 @@ export default function VehicleList() {
   }
 
   return (
-    <div data-testid="results" className="vehicle-list">
-      {vehicles.map((vehicle) => (
-        <VehicleCard key={vehicle.id} vehicle={vehicle} />
+    <ul className="vehicle-list">
+      {vehicles.map((vehicle, index) => (
+        <motion.li
+          className="vehicle-list__item"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration,
+            delay: index * STAGGER
+          }}
+        >
+          <VehicleCard vehicle={vehicle} />
+        </motion.li>
       ))}
-    </div>
+    </ul>
   );
 }
