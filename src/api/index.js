890f92428748a34faee893a158b1b7b49f003f21
Luca Arienti <lucarienti@gmail.com>
2022-07-11 10:26:52 +0200

    chore: api call to retrieve vehicle information and details

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,32 @@
-// eslint-disable-next-line no-unused-vars
+import Vehicle from '../entities/Vehicle';
 import { request } from './helpers';
 
 /**
  * Pull vehicles information
  *
- * @return {Promise<Array.<vehicleSummaryPayload>>}
+ * @return {Promise<Array.<Vehicle>>}
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  return [];
+  try {
+    const vehicles = await request('/api/vehicles.json');
+
+    // Run all promises in parallel
+    const vehiclesWithDetails = await Promise.all(vehicles.map(async(vehicle) => {
+      try {
+        const details = await request(vehicle.apiUrl);
+        return new Vehicle({
+          ...vehicle,
+          ...details,
+        });
+      } catch (err) {
+        return null;
+      }
+    }));
+
+    // Filters out vehicles with broken details or without price
+    return vehiclesWithDetails.filter((vehicle) => !!vehicle && !!vehicle.price);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
